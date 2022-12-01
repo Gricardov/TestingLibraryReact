11492a782b8828fdbedeeba17247f9004353fec6
gricardov <gricardov@gmail.com>
2022-12-01 00:14:28 -0500

    displaying toppings test

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 
-export default function ScoopOptions({ name, imagePath }) {
+export default function ScoopOption({ name, imagePath }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { text } from "express";
 import Options from "../Options";
 
 test("displays image for each scoop from the server", async () => {
@@ -9,4 +10,20 @@ test("displays image for each scoop from the server", async () => {
 
   const altTexts = scoopImages.map((e) => e.alt);
   expect(altTexts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+text("displays image for each topping from the server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const altTexts = toppingImages.map((e) => e.alt);
+  expect(altTexts).toEqual([
+    "Cherries topping",
+    "M&M topping",
+    "Hot fudge topping",
+  ]);
 });
